c744bbdfcb1bf58781bf4d98ef100f9e094e970f
tijn <tijnjh@proton.me>
2025-05-06 18:55:56 +0200

    feat: implement effetch function for effective data fetching

---
+++ src/effetch/index.ts
@@ -0,0 +1,51 @@
+import { tryCatch } from "..";
+
+type EffetchOptions = RequestInit & {
+  /** Primarily for SvelteKit's load function */
+  event?: Event & { fetch: typeof fetch };
+
+  responseType?: "json" | "text";
+};
+
+/**
+ *
+ * Effective fetch
+ *
+ * Sends the request and returns the response as parsed JSON (or text).
+ *
+ * @template T The expected type of the JSON response
+ * @returns A promise that resolves to the parsed JSON or text data
+ * @throws If the fetch request fails or if the JSON parsing fails
+ */
+export default async function effetch<T>(
+  input: RequestInfo | URL,
+  init?: EffetchOptions
+) {
+  const fetchFunction = init?.event?.fetch || fetch;
+
+  const [res, err] = await tryCatch(fetchFunction(input, init));
+
+  if (err) {
+    throw new Error(`Failed to fetch: ${err}`);
+  }
+
+  if (!res) {
+    throw new Error("Request sending failed unexpectedly.");
+  }
+
+  if (!res.ok) {
+    throw new Error(`HTTP error! status: ${res?.status} ${res?.statusText}`);
+  }
+
+  if (init?.responseType === "text") {
+    return res.text();
+  }
+
+  const [parseRes, parseErr] = await tryCatch(res.json());
+
+  if (parseErr) {
+    throw new Error(`Failed to parse JSON: ${parseErr}`);
+  }
+
+  return parseRes as T;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { default as createNode } from "./create-node/index";
 export { default as createRequest } from "./create-request/index";
 export { default as createState } from "./create-state/index";
+export { default as effetch } from "./effetch/index";
 export { default as mapElementsById } from "./map-elements-by-id/index";
 export { default as hugText } from "./hug-text/index";
 export { default as loadImageWithFallback } from "./load-image-with-fallback/index";
