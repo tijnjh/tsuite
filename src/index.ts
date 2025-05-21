942bf00793b3a6228a65e46b6fd384e598ff045c
tijn <tijn@tijn.dev>
2025-05-21 09:01:56 +0200

    fix: remove export

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,4 @@ export { default as effetch } from "./effetch/index";
 export { default as mapElementsById } from "./map-elements-by-id/index";
 export { default as hugText } from "./hug-text/index";
 export { default as loadImageWithFallback } from "./load-image-with-fallback/index";
-export { default as toast } from "./toast/index";
 export { default as tryCatch } from "./try-catch/index";
