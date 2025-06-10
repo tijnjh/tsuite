382d0c233e202a7ab9114c919325474d9bb1771e
tijn <tijn@tijn.dev>
2025-06-10 15:02:15 +0200

    docs: update license (less strict)

--- src/type-helpers.ts
+++ src/type-helpers.ts
@@ -2,11 +2,11 @@ declare const _brand: unique symbol;
 export type Brand<T, TBrand> = T & { [_brand]: TBrand };
 
 export type LooseToStrict<T> = T extends any ? string extends T ? never
-    : T
-    : never;
+  : T
+  : never;
 
 export type Prettify<T> =
-    & {
-        [K in keyof T]: T[K];
-    }
-    & {};
+  & {
+    [K in keyof T]: T[K];
+  }
+  & {};
--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,5 +1,13 @@
 import { defineConfig } from "tsdown/config";
 
+import pkg from "./package.json";
+
+const banner = `/**
+* ${pkg.name} v${pkg.version}
+* tijn.dev
+* @license MPL-2.0
+**/`;
+
 export default defineConfig({
   exports: true,
   entry: [
@@ -11,5 +19,8 @@ export default defineConfig({
     "src/hug-text.ts",
     "src/load-image-with-fallback.ts",
     "src/map-elements-by-id.ts",
+    "src/type-helpers.ts",
   ],
+  platform: "neutral",
+  outputOptions: { banner },
 });
